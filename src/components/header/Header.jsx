0b40130dca453c133cd4914203ec7e6733f9645c
Haph762 <phanha762@gmail.com>
2022-11-09 22:53:38 +0700

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import css from "./Header.module.css";
+import logo from "../../assets/logo.png";
+import { CgShoppingBag } from "react-icons/cg";
+const Header = () => {
+  return (
+    <div className={css.container}>
+      <div className={css.logo}>
+        <img src={logo} alt="logo" />
+        <span>Amazon</span>
+      </div>
+      <div className={css.right}>
+        <div className={css.menu}>
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>ENG</li>
+          </ul>
+        </div>
+        <input type="text" className={css.search} placeholder={"Search"} />
+        <CgShoppingBag className={css.cart} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
