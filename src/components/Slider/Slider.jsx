0fbdde525b173db8e56d5a70f3859baeba721e61
Haph762 <phanha762@gmail.com>
2022-11-11 21:05:36 +0700

    hero and sidler

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Slider.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation, Pagination } from "swiper";
+import { SliderProducts } from "../../data/products";
+const Slider = () => {
+  return (
+    <div className="s-container">
+      <Swiper
+        className="mySwiper"
+        navigation={true}
+        loopFillGroupWithBlank={true}
+        modules={[Navigation, Pagination]}
+        slidesPerView={3}
+        spaceBetween={40}
+        slidesPerGroup={1}
+        loop={true}
+      >
+        {SliderProducts.map((slide, i) => (
+          <SwiperSlide key={i}>
+            <div className="left-s">
+              <div className="name">
+                <span>{slide.name}</span>
+                <span>{slide.detail}</span>
+              </div>
+              <span>{slide.price}$</span>
+              <div>Shop now</div>
+            </div>
+            <img src={slide.img} alt="Product" className="img-p" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import css from "./Hero.module.css";
+import HeroImg from "../../assets/hero.png";
+import { RiShoppingBagFill } from "react-icons/ri";
+import { BsArrowRight } from "react-icons/bs";
+const Hero = () => {
+  return (
+    <div className={css.container}>
+      {/* Left side */}
+      <div className={css.h_sides}>
+        <span className={css.text1}>Skin protection cream</span>
+        <div className={css.text2}>
+          <span>Trendy Collection</span>
+          <span>
+            Seedily say has suitable disposal and boy. Exercise joy man children
+            rejoiced
+          </span>
+        </div>
+      </div>
+      {/* middle side hero image */}
+      <div className={css.wrapper}>
+        <div className={css.blueCircle}></div>
+        <img src={HeroImg} alt="heroImg" width={600} />
+        <div className={css.cart2}>
+          <RiShoppingBagFill />
+          <div className={css.signup}>
+            <span>Best Signup Offers</span>
+            <div>
+              <BsArrowRight />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Right side */}
+      <div className={css.h_sides}>
+        <div className={css.traffic}>
+          <span> 1.5m</span>
+          <span> Monthly Traffic</span>
+        </div>
+        <div className={css.customers}>
+          <span> 100k</span>
+          <span> Happy Customers</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
